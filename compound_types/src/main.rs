1ebb121d7ab9536cb442b95b5778769ef3b1e73c
Akhil Narang <me@akhilnarang.dev>
2021-01-24 17:00:43 +0530

    Tuples, arrays
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

---
+++ compound_types/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let test: (i16, f32, u8) = (200, 3.14, 5);
+    let (a, b, c) = test;
+    println!("a, b, c are: {}, {}, {}!", a, b, c);
+    println!("First value is {}", test.0);
+    println!("Second value is {}", test.1);
+    println!("Third value is {}", test.2);
+
+    let arr_test = [3, 7, 11];
+    println!("First value is {}", arr_test[0]);
+    println!("Second value is {}", arr_test[1]);
+    println!("Third value is {}", arr_test[2]);
+
+}
